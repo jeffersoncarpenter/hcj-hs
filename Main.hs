564f2737739904592846164e2e7685c3953dfeb2
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-11-08 20:39:43 -0600

    it can measure elements

--- Main.hs
+++ Main.hs
@@ -4,50 +4,99 @@ module Main (
 
 import Control.Applicative ((<$>))
 import Control.Monad (liftM)
+import Control.Monad.Trans (lift)
 import Data.Conduit
 import Data.Maybe (fromJust)
 import GHCJS.DOM
        (enableInspector, webViewGetDomDocument, runWebGUI)
-import GHCJS.DOM.Document (getBody, createElement, createTextNode, click)
-import GHCJS.DOM.Element (Element, setInnerHTML)
-import GHCJS.DOM.Node (appendChild, removeChild)
+import GHCJS.DOM.CSSStyleDeclaration (removeProperty, setProperty)
+import GHCJS.DOM.Document (getBody, createElement, createTextNode, click, getElementById)
+import GHCJS.DOM.Element (castToElement, Element, getScrollHeight, getScrollWidth, getStyle, setAttribute, setInnerHTML)
+import GHCJS.DOM.Node (appendChild, cloneNode, getOwnerDocument, removeChild)
 import GHCJS.DOM.EventM (on, mouseClientXY)
 import GHCJS.DOM.Types (Document, HTMLElement, Window)
 
+-- we never use !important
+setStyle a b c = setProperty a b c ""
+
+
+main :: IO ()
 main = runWebGUI $ \ webView -> do
     Just doc <- webViewGetDomDocument webView
-    Just body <- getBody doc
-    undo <- text "aoeu" doc body
-    on doc click $ do
-        (x, y) <- mouseClientXY
-        Just newParagraph <- createElement doc (Just "p")
-        text <- createTextNode doc $ "Click " ++ show (x, y)
-        appendChild newParagraph text
-        appendChild body (Just newParagraph)
-        return ()
+    initSandbox doc
+    undo <- rootComponent doc $ testComponent "aoeu"
+    on doc click $ lift undo
     return ()
 
+sandboxId = "sandbox"
 
-type Size = (Int, Int)
-type Position = (Int, Int)
+initSandbox :: Document -> IO ()
+initSandbox doc = do
+  Just sandbox <- createElement doc (Just "div")
+  Just body <- getBody doc
+  Just style <- getStyle sandbox
+  setAttribute sandbox "id" sandboxId
+  setStyle style "z-index" (Just "-1")
+  appendChild body (Just sandbox)
+  return ()
 
-data Instance = Instance { dimensions :: Source IO (Int, Int -> Int)
-                         , place :: (Size, Position) -> IO () }
 
-type Component = Document -> HTMLElement -> IO (IO ())
+type Dimensions = (Int, Int -> IO Int)
+
+data Instance = Instance { dimensions :: Source IO Dimensions
+                         , place :: (Int, Int) -> IO () }
+
+type Component = HTMLElement -> IO (Instance, IO ())
 
 type Layout = [Component] -> Component
 
+width :: String
+width = "width"
 
-text :: String -> Document -> HTMLElement -> IO (IO ())
-text str doc parent = do
+sandboxElement :: (Element -> IO a) -> Element -> IO a
+sandboxElement f el = do
+  Just doc <- getOwnerDocument el
+  Just sandbox <- getElementById doc sandboxId
+  Just el' <- (fmap . fmap) castToElement $ cloneNode el True
+  appendChild sandbox (Just el')
+  r <- f el'
+  removeChild sandbox (Just el')
+  return r
+
+measureElement :: Element -> IO Dimensions
+measureElement el = sandboxElement (\el' -> do
+  Just style <- getStyle el'
+  setStyle style "position" (Just "absolute")
+  removeProperty style "width" :: IO (Maybe String)
+  removeProperty style "height" :: IO (Maybe String)
+  w <- getScrollWidth el'
+  return (w, \w -> sandboxElement (\el' -> do
+                                       Just style <- getStyle el'
+                                       setStyle style "width" (Just $ show w ++ "px")
+                                       removeProperty style "height" :: IO (Maybe String)
+                                       getScrollHeight el') el)) $ el
+
+testComponent :: String -> Component
+testComponent str parent = do
+  Just doc <- getOwnerDocument parent
   (Just el) <- createElement doc (Just "div")
   text <- createTextNode doc str
   appendChild el text
   appendChild parent (Just el)
-  return $ do
-    removeChild parent (Just el)
-    return ()
+  return (Instance (do
+                        yield =<< (lift $ measureElement el)
+                        return ()) undefined, do
+              removeChild parent (Just el)
+              return ())
 
-rootComponent :: Window -> Component -> IO (IO ())
-rootComponent window c = undefined
+rootComponent :: Document -> Component -> IO (IO ())
+rootComponent doc c = do
+  Just body <- getBody doc
+  (Instance dS place, undo) <- c body
+  dS $$ do
+    Just (w, hF) <- await
+    lift (putStrLn $ show w)
+    h <- lift $ hF w
+    lift (putStrLn $ show h)
+    return ()
+  return undo
